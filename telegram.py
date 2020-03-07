566c23ee748cc33e1d7a0d05f662e56b1b20e216
Justin Ng <jng@slack-corp.com>
2020-03-07 12:54:23 -0700

    Add helper method to assemble webhook URL

--- telegram.py
+++ telegram.py
@@ -3,3 +3,7 @@ BASE_URI = 'https://api.telegram.org/bot'
 
 def assemble_uri(token, method_name):
     return BASE_URI + token + '/' + method_name
+
+
+def assemble_webhook_uri(base_uri, token):
+    return base_uri + '/api/v1/bots/updates/' + token
